1c63977830192ec239f08694ced7f925e5e5f613
Roy Sommer <roy@sommer.co.il>
2017-05-02 03:12:41 +0300

    Added scripts for integration tests

---
+++ scripts/integration_start.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+import os
+import shutil
+import subprocess
+
+
+if __name__ == '__main__':
+    executable_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..')
+    assets_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'tmp')
+    server_port = 12000
+
+    try:
+        shutil.rmtree(assets_path)
+    except:
+        pass
+
+    os.makedirs(assets_path)
+
+    subprocess.Popen('./start.py -p {0} -db "sqlite:///{1}"'.format(server_port,
+                                                                    os.path.join(assets_path, 'db.sqlite')),
+                     shell=True,
+                     cwd=executable_path).communicate()
